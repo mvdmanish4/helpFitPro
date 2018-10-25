d50b47c5bc7873d3320cbdd4a0495c0b48d5244c
Siva Shankaran <sivashav@andrew.cmu.edu>
2018-10-24 18:12:24 -0700

    Regime Program related APIs by Manish

---
+++ src/main/java/com/app/server/models/HealthRegime/RegimeProgram.java
@@ -0,0 +1,171 @@
+package com.app.server.models.HealthRegime;
+
+import java.util.Date;
+import java.util.List;
+
+public class RegimeProgram {
+
+    String id;
+    String userID;
+    List<String> regimesID;
+    Date recommendedDate;
+    List<String> ailmentsAddressed;
+    List<String> interestsAddressed;
+    List<String> habitsAddressed;
+    String isActive;
+    String isFulfilled;
+    Double durationWeeks;
+    Date startDate;
+    Date endDate;
+    Date timeCreated;
+    Date timeUpdated;
+
+    public RegimeProgram(String id, String userID, List<String> regimesID, Date recommendedDate, List<String> ailmentsAddressed, List<String> interestsAddressed, List<String> habitsAddressed, String isActive, String isFulfilled, Double durationWeeks, Date startDate, Date endDate, Date timeCreated, Date timeUpdated) {
+        this.id = id;
+        this.userID = userID;
+        this.regimesID = regimesID;
+        this.recommendedDate = recommendedDate;
+        this.ailmentsAddressed = ailmentsAddressed;
+        this.interestsAddressed = interestsAddressed;
+        this.habitsAddressed = habitsAddressed;
+        this.isActive = isActive;
+        this.isFulfilled = isFulfilled;
+        this.durationWeeks = durationWeeks;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.timeCreated = timeCreated;
+        this.timeUpdated = timeUpdated;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public List<String> getRegimesID() {
+        return regimesID;
+    }
+
+    public void setRegimesID(List<String> regimesID) {
+        this.regimesID = regimesID;
+    }
+
+    public Date getRecommendedDate() {
+        return recommendedDate;
+    }
+
+    public void setRecommendedDate(Date recommendedDate) {
+        this.recommendedDate = recommendedDate;
+    }
+
+    public List<String> getAilmentsAddressed() {
+        return ailmentsAddressed;
+    }
+
+    public void setAilmentsAddressed(List<String> ailmentsAddressed) {
+        this.ailmentsAddressed = ailmentsAddressed;
+    }
+
+    public List<String> getInterestsAddressed() {
+        return interestsAddressed;
+    }
+
+    public void setInterestsAddressed(List<String> interestsAddressed) {
+        this.interestsAddressed = interestsAddressed;
+    }
+
+    public List<String> getHabitsAddressed() {
+        return habitsAddressed;
+    }
+
+    public void setHabitsAddressed(List<String> habitsAddressed) {
+        this.habitsAddressed = habitsAddressed;
+    }
+
+    public String getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(String isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getIsFulfilled() {
+        return isFulfilled;
+    }
+
+    public void setIsFulfilled(String isFulfilled) {
+        this.isFulfilled = isFulfilled;
+    }
+
+    public Double getDurationWeeks() {
+        return durationWeeks;
+    }
+
+    public void setDurationWeeks(Double durationWeeks) {
+        this.durationWeeks = durationWeeks;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(Date timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public Date getTimeUpdated() {
+        return timeUpdated;
+    }
+
+    public void setTimeUpdated(Date timeUpdated) {
+        this.timeUpdated = timeUpdated;
+    }
+
+    @Override
+    public String toString() {
+        return "RegimeProgram{" +
+                "id='" + id + '\'' +
+                ", userID='" + userID + '\'' +
+                ", regimesID=" + regimesID +
+                ", recommendedDate=" + recommendedDate +
+                ", ailmentsAddressed=" + ailmentsAddressed +
+                ", interestsAddressed=" + interestsAddressed +
+                ", habitsAddressed=" + habitsAddressed +
+                ", isActive='" + isActive + '\'' +
+                ", isFulfilled='" + isFulfilled + '\'' +
+                ", durationWeeks=" + durationWeeks +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", timeCreated=" + timeCreated +
+                ", timeUpdated=" + timeUpdated +
+                '}';
+    }
+}
---
+++ src/main/java/com/app/server/services/RegimeService.java
@@ -0,0 +1,217 @@
+package com.app.server.services;
+
+import com.app.server.models.HealthRegime.Regime;
+import com.app.server.util.MongoPool;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class RegimeService {
+
+    private static RegimeService self;
+    private ObjectWriter ow;
+    private MongoCollection<Document> regimeCollection = null;
+
+    private RegimeService() {
+        this.regimeCollection = MongoPool.getInstance().getCollection("regime");
+        ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+    }
+
+    public static RegimeService getInstance(){
+        if (self == null)
+            self = new RegimeService();
+        return self;
+    }
+
+    public ArrayList<Regime> getAll() {
+        ArrayList<Regime> regimeList = new ArrayList<Regime>();
+
+        FindIterable<Document> results = this.regimeCollection.find();
+        if (results == null) {
+            return regimeList;
+        }
+        for (Document item : results) {
+            Regime regime = convertDocumentToRegime(item);
+            regimeList.add(regime);
+        }
+        return regimeList;
+    }
+
+    public List<Regime> getRegimeForOneRegimeProgram(List<String> regimesID){
+        List<Regime> regimeList = new ArrayList<Regime>();
+        FindIterable<Document> results = this.regimeCollection.find();
+        if (results == null) {
+            return regimeList;
+        }
+        for (Document item : results) {
+            Regime regime = convertDocumentToRegime(item);
+            if(regimesID.contains(regime.getHelpFitID())){
+                regimeList.add(regime);
+            }
+        }
+        return regimeList;
+    }
+
+    public Object getRegimeForOneRegimeProgramWhenSubresource(List<String> regimesID, String idTwo, Object request){
+        List<Regime> regimeList = new ArrayList<Regime>();
+        FindIterable<Document> results = this.regimeCollection.find();
+        Regime regUpdate; Object obj = null;
+        if (results == null) {
+            return regimeList;
+        }
+        for (Document item : results) {
+            Regime regime = convertDocumentToRegime(item);
+            if(regimesID.contains(regime.getHelpFitID())){
+                regimeList.add(regime);
+            }
+        }
+        for (Regime reg: regimeList) {
+            if(reg.getId().contains(idTwo)) {
+                regUpdate = reg;
+                obj = update(idTwo, request);
+                break;
+            }
+        }
+        return obj;
+    }
+
+    public Regime getOne(String id) {
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", new ObjectId(id));
+        Document item = regimeCollection.find(query).first();
+        if (item == null) {
+            return  null;
+        }
+        return convertDocumentToRegime(item);
+    }
+
+    public Regime create(Object request) {
+        try {
+            JSONObject json = null;
+            json = new JSONObject(ow.writeValueAsString(request));
+            Regime regime = convertJsonToRegime(json);
+            Document doc = convertRegimeToDocument(regime);
+            regimeCollection.insertOne(doc);
+            ObjectId id = (ObjectId)doc.get( "_id" );
+            regime.setId(id.toString());
+            return regime;
+        } catch(JsonProcessingException e) {
+            System.out.println("Failed to create a document");
+            return null;
+        }
+    }
+
+
+    public Object update(String id, Object request) {
+        try {
+            JSONObject json = null;
+            json = new JSONObject(ow.writeValueAsString(request));
+
+            BasicDBObject query = new BasicDBObject();
+            query.put("_id", new ObjectId(id));
+
+            Document doc = new Document();
+            if (json.has("name"))
+                doc.append("name",json.getString("name"));
+            if (json.has("description"))
+                doc.append("description",json.getString("description"));
+            if (json.has("regimeType"))
+                doc.append("regimeType",json.getString("regimeType"));
+            if (json.has("timeRequiredWeeks"))
+                doc.append("timeRequiredWeeks",json.getString("timeRequiredWeeks"));
+
+            Document set = new Document("$set", doc);
+            regimeCollection.updateOne(query,set);
+            return request;
+        } catch(JSONException e) {
+            System.out.println("Failed to update a document");
+            return null;
+        } catch(JsonProcessingException e) {
+            System.out.println("Failed to create a document");
+            return null;
+        }
+    }
+
+    public Object delete(String id) {
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", new ObjectId(id));
+
+        regimeCollection.deleteOne(query);
+
+        return new JSONObject();
+    }
+
+    private Document convertRegimeToDocument(Regime regime){
+        Document doc = new Document("helpFitID", "reg")
+                .append("name", regime.getName())
+                .append("description", regime.getDescription())
+                .append("regimeType", regime.getRegimeType())
+                .append("timeRequiredWeeks", regime.getTimeRequiredWeeks())
+                .append("ailmentTags", regime.getAilmentTags())
+                .append("interestTags", regime.getInsterestTags())
+                .append("habitsTags", regime.getHabitsTags())
+                .append("isActive", regime.getIsActive()).append("timeCreated", new Date()).append("timeUpdated", regime.getTimeUpdated());
+        return doc;
+    }
+
+    private Regime convertDocumentToRegime(Document item) {
+        List<String> ailments = (List<String>) item.get("ailmentTags");
+        List<String> interests = (List<String>) item.get("interestTags");
+        List<String> habits = (List<String>) item.get("habitsTags");
+        Regime regime = new Regime(item.getObjectId("_id").toString(),item.getString("helpFitID"),
+                item.getString("name"),
+                item.getString("description"),
+                item.getString("regimeType"),
+                item.getString("timeRequiredWeeks"),
+                ailments,
+                interests,
+                habits,
+                item.getString("isActive"),
+                item.getDate("timeCreated"),
+                item.getDate("timeUpdated"));
+        return regime;
+    }
+
+    private Regime convertJsonToRegime(JSONObject json){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Regime regime = new Regime( json.getString("id"),
+                json.getString("helpFitID"),
+                json.getString("name"),
+                json.getString("description"),
+                json.getString("regimeType"),
+                json.getString("timeRequiredWeeks"),
+                jsonArraytoList(json.getJSONArray("ailmentTags")),
+                jsonArraytoList(json.getJSONArray("insterestTags")),
+                jsonArraytoList(json.getJSONArray("habitsTags")),
+                json.getString("isActive"),
+                new Date(),
+                new Date());
+        return regime;
+    }
+
+    private List<String> jsonArraytoList(JSONArray jsonArray){
+        ArrayList<String> list = new ArrayList<String>();
+        if (jsonArray != null) {
+            int len = jsonArray.length();
+            for (int i=0;i<len;i++){
+                list.add(jsonArray.get(i).toString());
+            }
+        }
+        return list;
+    }
+}
